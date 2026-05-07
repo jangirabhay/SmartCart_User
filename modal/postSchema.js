d75983d9b965d8222f78365d916d67ad74cfb8ea
ABHAY <168776158+jangirabhay@users.noreply.github.com>
2026-05-08 00:33:33 +0530

    Add new fields and status to postSchema

--- modal/postSchema.js
+++ modal/postSchema.js
@@ -1,9 +1,30 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    name : { type: String, required: true },
-    phone : { type: Number, required: true },
-    category : { type: String, required: true },
+   name: { type: String, required: true },
+    phone: { type: Number, required: true },
+    category: { type: String, required: true },
+    productDetails: { type: String, required: true },
+    productDescription: { type: String, required: true },
+    location_coordinate: { type: Object },
+    display_location: { type: String },
+
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected', 'completed'],
+        default: 'pending',
+    },
+    
+    acceptedBy: {
+        sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        acceptedAt: { type: Date },
+    },
+    
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 300,
+    },
 });
 
 
