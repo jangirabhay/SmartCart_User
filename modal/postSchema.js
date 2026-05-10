ae3fb6c7efba978dc4238b82c3032450d4ee2274
ABHAY <168776158+jangirabhay@users.noreply.github.com>
2026-05-10 16:54:16 +0530

    Refactor postSchema to include createdBy and update fields

--- modal/postSchema.js
+++ modal/postSchema.js
@@ -1,33 +1,29 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-   name: { type: String, required: true },
-    phone: { type: Number, required: true },
-    category: { type: String, required: true },
-    productDetails: { type: String, required: true },
-    productDescriptions: { type: String, required: true },
-    location_coordinate: { type: Object },
-    streetAddress : { type: String },
-    radiusSearch : {type:String,required:true},
-   
-    status: {
-        type: String,
-        enum: ['pending', 'accepted', 'rejected', 'completed'],
-        default: 'pending',
-    },
-    
-    acceptedBy: {
-        sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        acceptedAt: { type: Date },
-    },
-    
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        expires: 300,
-    },
-});
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  category: { type: String, required: true },
+  productDetails: { type: String, required: true },
+  productDescription: { type: String, required: true },
+  location_coordinate: { type: Object },
+  display_location: { type: String },
+  radiusSearch: { type: Number },
 
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected", "completed"],
+    default: "pending",
+  },
+
+  acceptedBy: {
+    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    acceptedAt: { type: Date },
+  },
+});
 
 const Post = mongoose.model("Post", postSchema);
 
