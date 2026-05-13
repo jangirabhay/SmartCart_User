b1ae6bd34f45aeb0b296516111292bf2a1e5fdf9
ABHAY <168776158+jangirabhay@users.noreply.github.com>
2026-05-13 22:21:30 +0530

    Add default null to customer_id and seller_id fields
    Set default value of customer_id and seller_id to null

--- modal/postSchema.js
+++ modal/postSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
 
-customer_id: { type: Object},
+customer_id: { type: Object,default : null},
   category: { type: String, required: true },
   productDetails: { type: String, required: true },
   productDescription: { type: String, required: true },
@@ -16,7 +16,7 @@ customer_id: { type: Object},
     default: "pending",
   },
 
-seller_id: { type: Object}
+seller_id: { type: Object,default : null}
 },
   {
     timestamps: true,
